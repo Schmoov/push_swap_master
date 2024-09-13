a648f14a235237e3fbca86f69510f4051595ae4a
Paul Arden <parden@paul-f4Br2s1.clusters.42paris.fr>
2024-09-13 20:29:40 +0200

    V1 of big sort we got 125 already

--- src/solve_big.c
+++ src/solve_big.c
@@ -3,20 +3,23 @@
 void	solve_big_divide(t_stk *a, t_stk *b, t_sol *sol)
 {
 	int	i;
-	int	len;
+	int	lo;
+	int	mid;
 
-	while (a->len > 2)
+	while (a->len > 1)
 	{
 		i = 0;
-		len = a->len;
+		int len = a->len;
+		lo = b->len + a->len / 4 ;
+		mid = b->len + a->len / 2 ;
 		while (i < len)
 		{
-			if (a->head->val >= 3 * len / 4)
+			if (a->head->val <= lo)
 			{
 				ps_op_pb(a,b);
 				sol_append_one(sol, OP_PB);
 			}
-			else if (a->head->val >= len / 2)
+			else if (a->head->val <= mid)
 			{
 				ps_op_pb(a,b);
 				ps_op_rb(a,b);
@@ -31,45 +34,118 @@ void	solve_big_divide(t_stk *a, t_stk *b, t_sol *sol)
 			i++;
 		}
 	}
-	if (a->head->val)
+}
+
+int		insert_cost(int pos, t_stk *a, t_stk *b)
+{
+	int res = (pos <= b->len - pos ? pos : b->len - pos);
+	int posA = -1;
+	int posMin;
+	int val = b->head->val;
+
+	for (int i = 0; i < a->len; i++)
 	{
-		ps_op_sa(a,b);
-		sol_append_one(sol, OP_SA);
+		if (posA == -1 && a->head->val > val && a->head->prev->val < val)
+			posA = i;
+		if (a->head->val < a->head->prev->val)
+			posMin = i;
+		ps_op_ra(a,b);
 	}
+	if (posA == -1) posA = posMin;
+	if (a->len - posA < posA) posA = a->len - posA;
+	res += posA;
+	return (res);
+}
+
+void	fast_insert(int pos, t_stk *a, t_stk *b, t_sol *sol)
+{
+	if (pos <= b->len - pos)
+	{
+		for (int i = 0; i < pos; i++)
+		{
+			ps_op_rb(a,b);
+			sol_append_one(sol, OP_RB);
+		}
+	}
+	else
+	{
+		for (int i = 0; i < b->len - pos; i++)
+		{
+			ps_op_rrb(a,b);
+			sol_append_one(sol, OP_RRB);
+		}
+	}
+
+	int val = b->head->val;
+	int posA = -1;
+	int posMin;
+	for (int i = 0; i < a->len; i++)
+	{
+		if (posA == -1 && a->head->val > val && a->head->prev->val < val)
+			posA = i;
+		if (a->head->val < a->head->prev->val)
+			posMin = i;
+		ps_op_ra(a,b);
+	}
+	if (posA == -1) posA = posMin;
+	if (posA <= a->len - posA)
+	{
+		for (int i = 0; i < posA; i++)
+		{
+			ps_op_ra(a,b);
+			sol_append_one(sol, OP_RA);
+		}
+	}
+	else
+	{
+		for (int i = 0; i < a->len - posA; i++)
+		{
+			ps_op_rra(a,b);
+			sol_append_one(sol, OP_RRA);
+		}
+	}
+	ps_op_pa(a,b);
+	sol_append_one(sol, OP_PA);
 }
 
 void	solve_big_merge(t_stk *a, t_stk *b, t_sol *sol)
 {
-	int	pos;
-
+	ps_op_pa(a,b);
+	sol_append_one(sol, OP_PA);
+	ps_op_pa(a,b);
+	sol_append_one(sol, OP_PA);
 	while (b->len)
 	{
+		int	pos;
+		int best = INT_MAX;
 		for (int i = 0; i < b->len; i++)
 		{
-			if (b->head->val == a->len)
+			int cost = insert_cost(i, a, b);
+			if (cost < best)
+			{
 				pos = i;
+				best = cost;
+			}
 			ps_op_rb(a,b);
 		}
-		if (pos <= b->len / 2)
+		fast_insert(pos, a, b, sol);
+	}
+}
+
+void	solve_big_last_rot(t_stk *a, t_stk *b, t_sol *sol)
+{
+	while (a->head->val)
+	{
+		if (a->head->val < a->len / 2)
 		{
-			for (int i = 0; i < pos; i++)
-			{
-				ps_op_rb(a,b);
-				sol_append_one(sol, OP_RB);
-			}
+			ps_op_rra(a,b);
+			sol_append_one(sol, OP_RRA);
 		}
 		else
 		{
-			for (int i = 0; i < b->len - pos; i++)
-			{
-				ps_op_rrb(a,b);
-				sol_append_one(sol, OP_RRB);
-			}
+			ps_op_ra(a,b);
+			sol_append_one(sol, OP_RA);
 		}
-		ps_op_pa(a,b);
-		ps_op_ra(a,b);
-		sol_append_one(sol, OP_PA);
-		sol_append_one(sol, OP_RA);
 	}
 }
 
@@ -83,5 +159,6 @@ t_sol	solve_big(t_node *parsed, int size)
 	stks_init(parsed, size, &stk_a, &stk_b);
 	solve_big_divide(&stk_a, &stk_b, &sol);
 	solve_big_merge(&stk_a, &stk_b, &sol);
+	solve_big_last_rot(&stk_a, &stk_b, &sol);
 	return (sol);
 }
